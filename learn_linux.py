147f8269048b5e27bdc81b0bd406a6a8d3fbdb24
xF14x <>
2021-05-06 00:27:03 +0300

    Clean and Develop The Code

--- learn_linux.py
+++ learn_linux.py
@@ -3,12 +3,13 @@ from os import system
 # =-=-=-=-=-=-=-=-=-=
 system("clear")
 print(Fore.RED)
-print("""  _                         _ _                  
+print("""
+  _                         _ _                  
  | | ___  __ _ _ __ _ __   | (_)_ __  _   ___  __
  | |/ _ \/ _` | '__| '_ \  | | | '_ \| | | \ \/ / github(xF14x)
  | |  __/ (_| | |  | | | | | | | | | | |_| |>  <  twitter:(F14Commander)
  |_|\___|\__,_|_|  |_| |_| |_|_|_| |_|\__,_/_/\_\ youtube:(F14Commander سليمان المهوس)
- """)
+""")
 print(Fore.BLUE)
 tou = "1"
 mkdir = "2"
@@ -19,29 +20,31 @@ cp = "6"
 mv = "7"
 rm = "8"
 ls = "9"
-print("(1) كيف تسوي ملف")
-print("")
-print("(2) كيف تسوي مجلد")
-print("")
-print("(3) nano كيف تعدل ب")
-print("")
-print("(4) echo كيف تزيد كلام ب")
-print("")
-print("(5) echo كيف تسوي ملف ب")
-print("")
-print("(6) كيف تنسخ ملف او مجلد")
-print("")
-print("(7) كيف تنقل او تغير إسم ملف او مجلد")
-print("")
-print("(8) كيف تحذف ملف او مجلد")
-print("")
-print("(9) كيف تظهر الي داخل الملف")
-print("")
-print("")
+print("""
+(1) كيف تسوي ملف
+
+(2) كيف تسوي مجلد
+
+(3) nano كيف تعدل ب
+
+(4) echo كيف تزيد كلام ب
+
+(5) echo كيف تسوي ملف ب
+
+(6) كيف تنسخ ملف او مجلد
+
+(7) كيف تنقل او تغير إسم ملف او مجلد
+
+(8) كيف تحذف ملف او مجلد
+
+(9) كيف تظهر الي داخل الملف
+
+""")
 print(Fore.WHITE)
-text = input("??:")
+Number_Of_The_Command = input("??:")
+# =-=-=-=-=-=-=-=-=-=-=-=-=-=
 print(Fore.GREEN)
-if text == tou:
+if Number_Of_The_Command == tou:
     print("""touch (إسم الملف.txt)
 
 . وإذا كنت تبي الملف يكون مخفي أكتب قبله إسمة
@@ -53,7 +56,7 @@ if text == tou:
 .py
 
 وبس""")
-if text == mkdir:
+if Number_Of_The_Command == mkdir:
     print("""عشان تسوي مجلد اكتب
 
 mkdir (إسم المجلد)
@@ -62,7 +65,7 @@ mkdir (إسم المجلد)
 نقطة
 
 وبس""")
-if text == nano:
+if Number_Of_The_Command == nano:
     print("""أكتب nanoعشان تعدل النص ب
 
 nano (إسم الملف)
@@ -75,29 +78,29 @@ y عشان تحفظ
 n عدم الحفظ
 
 contro + c للرجوع للنص""")
-if text == echo:
+if Number_Of_The_Command == echo:
     print("""عشان تزيد على الملف كلام 
 
 echo إسم الملف<<  الكلام
 وبس""")
-if text == echo1:
+if Number_Of_The_Command == echo1:
     print("""عشان تسوي ملف  وتكتب فيه الكلام
 
 echo إسم الملف < الكلام
 وبس""")
-if text == cp:
+if Number_Of_The_Command == cp:
     print("""عشان تنسخ اي ملف او مجلد
 
 cp (إسم الملف او المجلد) (الإسم الجديد) (اللوكيشن)
 
 وبس""")
-if text == mv:
+if Number_Of_The_Command == mv:
     print("""كيف أنقل او أغير إسم الملف او المجلد
 
 mv  (إسم المجلد او الملف) (الإسم الجديد) (اللوكيشن إذا كنت تبي تنقلة)
 
 وبس""")
-if text == rm:
+if Number_Of_The_Command == rm:
     print("""كيف أحذف الملف او المجلد
 
 إذا ملف
@@ -108,7 +111,7 @@ rm (إسم المجلد)
 
 rm -r (إسم المجلد)
 وبس""")
-if text == ls:
+if Number_Of_The_Command == ls:
     print("""كيف أظهر اللي داخل الملف
 
 ls
